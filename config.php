c461cbd2a3ad88b73c133a9097fa8601d5d6a06c
Aalyan-butt <aalyanriasat@gmail.com>
2024-08-25 19:23:30 +0500

    add

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$conn = mysqli_connect ("localhost" , "root", "", "crud") or die("connection failed");
+
+?>
+
+<H1></H1>
---
+++ savedata.php
@@ -0,0 +1,20 @@
+<?php
+
+ $stu_name =   $_POST ['sname'];
+ $stu_adress = $_POST ['sadress'];
+ $stu_class =  $_POST ['sclass'];
+ $stu_phone =  $_POST ['sphone'];
+
+ include 'config.php';
+$sql = "INSERT INTO student(sname, sadress, sclass,sphone) VALUES('{$stu_name}', '{$stu_adress}','{$stu_class}','{$stu_phone}' )";
+$result = mysqli_query($conn, $sql) or die("Query unsuccessful"); 
+
+ header("Location: http://localhost/crud_html/index.php");
+
+mysqli_close($conn);
+
+
+
+?>
+
+<h1></h1>
